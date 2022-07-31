95f3d1a5c85ac11fe438ebbd36ba1c4498671889
dk-xiao <1915231131@qq.com>
2022-07-31 11:19:27 +0800

    front

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,7 +1,12 @@
 import { createWebHistory, createRouter } from "vue-router";
+import loginPage from "@/components/loginPage";
 
 const routes = [
-
+    {
+        path: '/',
+        name: 'login',
+        component: loginPage
+    }
 ];
 
 const router = createRouter({
