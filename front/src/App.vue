e7b1933d12f578c67c0fc037dd8fd1042eb69c2d
dk-xiao <1915231131@qq.com>
2022-07-31 11:06:37 +0800

    front

---
+++ front/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+body,html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ front/src/components/loginPage.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "loginPage",
+}
+</script>
+
+<style scoped>
+
+</style>
